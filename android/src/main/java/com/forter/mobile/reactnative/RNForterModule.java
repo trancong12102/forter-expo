228384fe31997e5fa5f678d615ca38b3a5110549
Or Polaczek <or.polaczek@forter.com>
2018-06-14 15:44:01 +0300

    Initial version

---
+++ android/src/main/java/com/forter/mobile/reactnative/RNForterModule.java
@@ -0,0 +1,174 @@
+package com.forter.mobile.reactnative;
+
+
+import android.app.Application;
+import android.location.Location;
+import android.os.Build;
+
+
+import com.forter.mobile.fortersdk.ForterSDK;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.forter.mobile.fortersdk.integrationkit.ForterIntegrationUtils;
+import com.forter.mobile.fortersdk.interfaces.IForterSDK;
+import com.forter.mobile.fortersdk.models.ForterAccountIDType;
+import com.forter.mobile.fortersdk.models.NavigationType;
+import com.forter.mobile.fortersdk.models.TrackType;
+import com.forter.mobile.fortersdk.utils.SDKLogger;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.forter.mobile.reactnative.RNForterConstants.*;
+
+public class RNForterModule extends ReactContextBaseJavaModule  {
+
+    private ReactApplicationContext reactContext;
+    private Application application;
+
+    public RNForterModule(ReactApplicationContext reactContext, Application application) {
+        super(reactContext);
+        this.reactContext = reactContext;
+        this.application = application;
+    }
+
+    @Override
+    public String getName() {
+        return "RNForter";
+    }
+
+    @ReactMethod
+    public void initSdk(
+            String siteId,
+            String mobileUid,
+            Callback successCallback,
+            Callback errorCallback
+            ) {
+
+        try {
+
+            if (siteId == null || siteId.isEmpty()) {
+                errorCallback.invoke(new Exception(NO_SITE_ID_FOUND).getMessage());
+                return;
+            }else if (mobileUid == null || mobileUid.isEmpty()) {
+                errorCallback.invoke(new Exception(NO_MOBILE_UID_FOUND).getMessage());
+                return;
+            }
+
+            sdk().init(this.application, siteId, mobileUid);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+            {
+                this.application.registerActivityLifecycleCallbacks(sdk().getActivityLifecycleCallbacks());
+            }
+
+            sdk().trackAction(TrackType.APP_ACTIVE, "RNForter");
+            successCallback.invoke(SUCCESS);
+        } catch (Exception e) {
+            errorCallback.invoke(e.getMessage());
+        }
+    }
+
+    @ReactMethod
+    public void getDeviceUniqueID(Callback callback) {
+        callback.invoke(ForterIntegrationUtils.getDeviceUID(this.application));
+    }
+
+    @ReactMethod
+    public void setAccountIdentifier(String accountIdentifier, String accountType) {
+        ForterAccountIDType type;
+        try {
+            type = ForterAccountIDType.valueOf(accountType);
+        } catch (IllegalArgumentException ex) {
+            type = ForterAccountIDType.OTHER;
+        }
+        sdk().setAccountUID(type, accountIdentifier);
+    }
+
+    @ReactMethod
+    public void trackNavigation(String screenName, String navigationType) {
+        sdk().trackNavigation(getMatchingNavigationType(navigationType),
+                screenName);
+    }
+
+    @ReactMethod
+    public void trackNavigationWithExtraData(String screenName, String navigationType, String itemId, String itemCategory, String otherInfo) {
+        sdk().trackNavigation(getMatchingNavigationType(navigationType),
+                screenName,
+                itemId,
+                itemCategory,
+                otherInfo);
+    }
+
+    @ReactMethod
+    public void trackAction(String actionType) {
+        sdk().trackAction(getMatchingActionType(actionType));
+    }
+
+    @ReactMethod
+    public void trackActionWithMessage(String actionType, String message) {
+        sdk().trackAction(getMatchingActionType(actionType),
+                message);
+    }
+
+    @ReactMethod
+    public void trackActionWithJSON(String actionType, ReadableMap actionData) {
+        JSONObject data = RNUtil.readableMapToJson(actionData);
+        if (data == null) { // In case of no values
+            data = new JSONObject();
+        }
+
+        sdk().trackAction(getMatchingActionType(actionType), data);
+    }
+
+    @ReactMethod
+    public void trackCurrentLocation(float longitude, float latitude) {
+        JSONObject data = new JSONObject();
+        try {
+            data.put("eventType", "locationUpdate");
+            data.put("latitude", latitude);
+            data.put("longitude", longitude);
+        } catch (JSONException e) {
+            return;
+        }
+        sdk().trackAction(TrackType.OTHER, data);
+    }
+
+    @ReactMethod
+    public void setDevLogsEnabled() {
+        SDKLogger.setDevLogsEnabled(true);
+    }
+
+    @ReactMethod
+    public void getSDKVersionSignature(Callback callback) {
+        callback.invoke(ForterSDK.BUILD_SIGNATURE);
+    }
+
+    private NavigationType getMatchingNavigationType(String type) {
+        try {
+            return NavigationType.valueOf(type);
+        } catch (IllegalArgumentException ex) {
+            return NavigationType.APP;
+        }
+    }
+
+    private TrackType getMatchingActionType(String type) {
+        try {
+            return TrackType.valueOf(type);
+        } catch (IllegalArgumentException ex) {
+            return TrackType.OTHER;
+        }
+    }
+
+    private IForterSDK sdk() {
+        return ForterSDK.getInstance();
+    }
+}
